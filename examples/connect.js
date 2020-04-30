6437a93926e35a57dfcdca6074c8e2189ae62c50
cseitz5 <cseitz5@kent.edu>
2020-04-30 19:56:23 -0400

    idk

--- examples/connect.js
+++ examples/connect.js
@@ -1,9 +1,9 @@
 // Example code for a nodejs program that is connecting to an already hosted IPC socket.
 
-var io = require("socketio-ipc").connect(); // first paramater can be a port, defaults to 57125
+var socket = require("socketio-ipc").connect(); // first paramater can be a port, defaults to 57125
 // socketio-ipc.connect() returns a socket.io client. https://socket.io/docs/client-api/
 
-io.on("connection", function(socket) {
+socket.on("connect", function() {
   console.log("Connected to the host");
   socket.emit("coolEvent", {
     yeet: "we sent some data",
